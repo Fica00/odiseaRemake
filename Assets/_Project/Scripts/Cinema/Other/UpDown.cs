de44b1d5e9e800cbe8b67a7b9c21a093589ddf6c
Petar S. <petar.stosic@magmadev.co>
2024-05-20 20:10:15 +0200

    Added Themes and small fixes

---
+++ Assets/_Project/Scripts/Cinema/Other/UpDown.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace ARspace
+{
+	public class UpDown : MonoBehaviour
+	{
+		public float radius = 1f;
+		public float speed = 1f;
+
+		private float y;
+		private float o;
+		private float a;
+
+		private void Awake()
+		{
+			y = transform.localPosition.y;
+			o = 0f;
+			a = 0f;
+		}
+		private void Update()
+		{
+			a += speed*Time.deltaTime;
+			o = Mathf.Sin(a)*radius;
+			transform.localPosition = new Vector3(transform.localPosition.x, y+o, transform.localPosition.z);
+		}
+	}
+}
